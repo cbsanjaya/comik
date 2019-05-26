fd5bd3db4c2df71c8b0a35d70ed64d6bc24e46f6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-05-27 00:06:31 +0700

    request over https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://komik.yudharta.ac.id/api/get.php'
+axios.defaults.baseURL = 'https://komik.yudharta.ac.id/api/get.php'
 
 export default async ({ Vue }) => {
   Vue.prototype.$axios = axios
